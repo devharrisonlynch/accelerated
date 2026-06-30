abc06f63f56d98949495e787990f7bc70baf03f6
Harrison Lynch <devharrisonlynch@users.noreply.github.com>
2026-06-30 20:05:21 +0300

    Wire program instructions

---
+++ programs/accelerated/src/instructions/mod.rs
@@ -0,0 +1,17 @@
+pub mod close_position;
+pub mod crank_funding;
+pub mod deposit_liquidity;
+pub mod initialize_market;
+pub mod initialize_protocol;
+pub mod liquidate;
+pub mod open_position;
+pub mod withdraw_liquidity;
+
+pub use close_position::*;
+pub use crank_funding::*;
+pub use deposit_liquidity::*;
+pub use initialize_market::*;
+pub use initialize_protocol::*;
+pub use liquidate::*;
+pub use open_position::*;
+pub use withdraw_liquidity::*;
